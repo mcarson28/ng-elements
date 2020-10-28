534b81b16798742dab3cd64952617ea640b8fd10
Matt Carson <mcarson28@gmail.com>
2020-10-28 11:56:07 -0400

    created component and app module changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const button = createCustomElement(ButtonComponent, {injector: this.injector})
+    customElements.define('ngair-button', button)
+  }
+}

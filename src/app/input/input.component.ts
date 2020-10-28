ad2a78cbbe156e8dc152272df889f2edf31993e2
Matt Carson <mcarson28@gmail.com>
2020-10-28 12:47:00 -0400

    adding slots

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class InputComponent implements OnInit {
 
